c320b77947979ebd98dd815b9d007b8812985d8b
Ro-shni <112896479+Ro-shni@users.noreply.github.com>
2024-10-16 16:23:02 +0530

    Update user.js CSS

--- pages/user.js
+++ pages/user.js
@@ -9,15 +9,16 @@ import WhereToButton from "./Index_Page_Components/WhereToButton";
 const User = () => {
   return (
     <Wrapper>
-      <Map />
+      <Header />
+      <MapWrapper>
+        <Map />
+      </MapWrapper>
       <ActionItems>
-        <Header />
         <ActionButtons>
           <ActionButtonContainer>
             <ActionButton
               src="/images/box-truck.jpg"
               type="Book"
-
             />
             <ActionLabel>Book</ActionLabel>
           </ActionButtonContainer>
@@ -26,7 +27,6 @@ const User = () => {
             <ActionButton
               src="/images/statusimg.jpeg"
               type="Status"
-
             />
             <ActionLabel>Status</ActionLabel>
           </ActionButtonContainer>
@@ -35,13 +35,10 @@ const User = () => {
             <ActionButton
               src="/images/reserveimg.jpeg"
               type="Reserve"
-
             />
             <ActionLabel>Reserve</ActionLabel>
           </ActionButtonContainer>
         </ActionButtons>
-
-        <WhereToButton />
       </ActionItems>
     </Wrapper>
   );
@@ -53,15 +50,24 @@ const Wrapper = tw.div`
   h-screen
 `;
 
-const ActionItems = tw.div`
+const MapWrapper = tw.div`
   flex-1
+`;
+
+const ActionItems = tw.div`
   p-5
+  flex 
+  justify-center 
+  items-end
+  pb-6  // adds a little gap at the bottom
 `;
 
 const ActionButtons = tw.div`
   flex
   justify-between
   gap-4
+  w-full
+  max-w-md  // limits the width of the buttons to look clean
 `;
 
 const ActionButtonContainer = tw.div`
